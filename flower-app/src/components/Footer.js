5e7dd92bb6395e8dc76bb90f058ce079a107c002
isha-ghosh <140252463+isha-ghosh@users.noreply.github.com>
2025-02-17 16:17:01 +0530

    Delete flower-app directory

--- flower-app/src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "../styles.css";
-
-function Footer() {
-  return <footer className="footer">&copy; 2025 All Rights Reserved.</footer>;
-}
-
-export default Footer;
